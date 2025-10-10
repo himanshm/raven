448796bbfd49a8fa85cbbc84c773b9b648e6ba65
Himanshu Mishra <sai.himanshum011@gmail.com>
2025-10-11 00:54:20 +0530

    initial commit-scaffold front end of finance folio

---
+++ apps/raven/vite.config.ts
@@ -0,0 +1,18 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
+  optimizeDeps: {
+    exclude: ["eslint", "eslint-plugin-import"]
+  }
+});
---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+import js from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  globalIgnores(['dist', 'vite.config.js']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    plugins: {
+      prettier: prettierPlugin,
+      import: importPlugin
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module'
+      }
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'import/no-duplicates': 'error',
+      'prettier/prettier': [
+        'warn',
+        {
+          arrowParens: 'avoid',
+          printWidth: 80,
+          semi: true,
+          trailingComma: 'none'
+        }
+      ]
+    }
+  },
+])
